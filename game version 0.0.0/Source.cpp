0788938d6750666b64b2b95b663fca3bf103b1e5
JARATRAWEE SRIJANSUK <63010120@KMITL.AC.TH>
2020-10-12 11:12:03 +0700

    เปลี่ยนมาเป็นทำเเลป

--- game version 0.0.0/Source.cpp
+++ game version 0.0.0/Source.cpp
@@ -1,46 +1,40 @@
-#include <SFML/Graphics.hpp>
-#include <stdio.h>
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+#include<conio.h>
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void draw_ship(int x, int y)
+{
+	gotoxy(x, y); printf(" <-0-> ");
+}
+void draw_bullet(int x, int y)
+{
+	gotoxy(x, y); printf("^");
+}
+void clear_bullet(int x, int y)
+{
+	gotoxy(x, y); printf(" ");
+}
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1080, 720), "Game from scratch!");
-	////// Texture
-	sf::Texture playerTexture;
-	playerTexture.loadFromFile("submarine1.png");
-	if (!playerTexture.loadFromFile("submarine1.png"))
+	int i,j,k;
+	srand(time(NULL));
+	for (i = 0;i < 20;i++)
 	{
-		printf("Load failed");
+		for (k = 0;k <= 5;k++) {
+			for (j = 0;j < 70;j++)
+			{
+				if ((k>=2&&k<=5)&&(j>=10&&j<=70)) printf("*", rand()); break;
+			}
+		printf("\n");
+		}
+		
 	}
-
-	////// Sprite
-	sf::Sprite shapeSprite;
-	shapeSprite.setTexture(playerTexture);
-	shapeSprite.setTextureRect(sf::IntRect(0, 0, 200, 200));
-
-	while (window.isOpen())
-	{
-		window.draw(shapeSprite);
-		window.display();
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			shapeSprite.move(.5f, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
-			shapeSprite.move(-.5f, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-		{
-			shapeSprite.move(0.f, -.5f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			shapeSprite.move(0.f, .5f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
-		window.clear();
-	}
+		
 	return 0;
 }
+	
